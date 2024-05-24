fbb3ef40e257342360b551d7ce50f59c365fd190
RJTutorials <riyakp67@gmail.com>
2024-05-24 22:09:54 +0530

    deploy

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Learn Git website loaded');
+    // Additional interactive JavaScript can go here
+});
+function redirectToAdvanced() {
+    window.location.href = "advanced.html"; // Redirect to advanced commands page
+}
+function goToHomePage() {
+    window.location.href = "index.html"; // Redirect to home page
+}
